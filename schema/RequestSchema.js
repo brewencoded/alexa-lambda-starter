771c548211d3e11f5d9fd0125f85fe9f5bdea67f
Victor Rodriguez <victor.rodriguez@uicentric.com>
2017-03-31 12:23:32 -0500

    passed tests for version in session

--- schema/RequestSchema.js
+++ schema/RequestSchema.js
@@ -9,7 +9,7 @@ const version = Joi.string().min(3).required();
 const session = Joi.object().keys({
     new: Joi.boolean().required(),
     sessionId: Joi.string().required(),
-    application: Joi.object.keys({
+    application: Joi.object().keys({
         applicationId: Joi.string().required() // whitelist your skill
     }).required(),
     attributes: Joi.object().required().min(1),
@@ -17,9 +17,10 @@ const session = Joi.object().keys({
         userId: Joi.string().required().max(255),
         accessToken: Joi.string().optional() // only if the user has linked their account
     }).required()
-}).when({
-    // do not allow with AudioPlayer or PlaybackController
 });
+// .when({
+//     // do not allow with AudioPlayer or PlaybackController
+// });
 
 module.exports = Joi.object().keys({
     version,
---
+++ test/RequestSchema/RequestSchema.spec.js
@@ -0,0 +1,24 @@
+const {
+    expect
+} = require('chai');
+const Joi = require('joi');
+const Request = require('../../schema/RequestSchema');
+
+describe('RequestSchema', () => {
+    describe('version', () => {
+        it('should not have errors with a valid version', () => {
+            Joi.validate({
+                version: '1.0'
+            },
+            Request,
+            (err) => expect(err).to.be.null);
+        });
+        it('should return ValidationError for an invalid version', () => {
+            Joi.validate({
+                version: 1.0
+            },
+            Request,
+            (err) => expect(err.name).to.equal('ValidationError'));
+        });
+    });
+});
