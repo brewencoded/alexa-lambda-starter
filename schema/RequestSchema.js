4ec135be91df99263304882e6161b4c8aa6d016b
Victor Rodriguez <victor.rodriguez@uicentric.com>
2017-04-03 16:59:12 -0500

    added request Joi schema and constants

--- schema/RequestSchema.js
+++ schema/RequestSchema.js
@@ -1,5 +1,9 @@
 const Joi = require('joi');
 const {
+    PLAYER_ACTIVITY,
+    INTENT_REQUEST,
+    LAUNCH_REQUEST,
+    SESSION_ENDED_REQUEST
 } = require('../constants/SchemaConstants');
 
 // version specifier for the request format: x.x
@@ -18,11 +22,70 @@ const session = Joi.object().keys({
         accessToken: Joi.string().optional() // only if the user has linked their account
     }).required()
 });
-// .when({
-//     // do not allow with AudioPlayer or PlaybackController
-// });
-
+// A system object that provides information about the current state of the Alexa service and the device interacting with your skill.
+const System = Joi.object().keys({
+    application: Joi.object().keys({
+        applicationId: Joi.string().required()
+    }),
+    user: Joi.object().keys({
+        userId: Joi.string().required().max(255),
+        accessToken: Joi.string().optional() // only if the user has linked their account
+    }),
+    device: Joi.object().keys({
+        supportedInterfaces: Joi.object()
+    }),
+});
+// Provides the current state for the AudioPlayer interface.
+const AudioPlayer = Joi.object().keys({
+    token: Joi.string(),
+    offsetInMilliseconds: Joi.number(),
+    playerActivity: Joi.string().valid(PLAYER_ACTIVITY.IDLE, PLAYER_ACTIVITY.PAUSED, PLAYER_ACTIVITY.PLAYING,
+        PLAYER_ACTIVITY.BUFFER_UNDERRUN, PLAYER_ACTIVITY.FINISHED, PLAYER_ACTIVITY.STOPPED)
+});
+// Provides your skill with information about the current state of the Alexa service and device at the time the request is sent to your service.
+// This is included on all requests.
+const context = Joi.object().keys({
+    System,
+    AudioPlayer
+});
+// A request object that provides the details of the user’s request.
+const request = Joi.object().keys({
+    type: Joi.string().allow(INTENT_REQUEST, SESSION_ENDED_REQUEST, LAUNCH_REQUEST),
+    requestId: Joi.string(),
+    timestamp: Joi.string(),
+    locale: Joi.string(),
+    intent: Joi.object()
+        .keys({
+            name: Joi.string().required(),
+            slots: Joi.object().optional()
+        })
+        .when('type', {
+            is: INTENT_REQUEST,
+            then: Joi.required(),
+            otherwise: Joi.forbidden()
+        }),
+    reason: Joi.string()
+        .when('type', {
+            is: SESSION_ENDED_REQUEST,
+            then: Joi.required(),
+            otherwise: Joi.forbidden()
+        }),
+    error: Joi.object()
+        .keys({
+            type: Joi.string().required(),
+            message: Joi.string().optional()
+        })
+        .when('type', {
+            is: SESSION_ENDED_REQUEST,
+            then: Joi.optional(),
+            otherwise: Joi.forbidden()
+        })
+});
 module.exports = Joi.object().keys({
     version,
-    session
-}).options({ convert: false });
+    session,
+    context,
+    request
+})
+.without('context.AudioPlayer', ['session'])
+.options({ convert: false });
